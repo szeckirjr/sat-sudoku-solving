e75aa996ac095aa4176c4959c466255fbc0e2e94
Rebecca Marshall <rebecca.marshall@shaw.ca>
2023-02-13 17:28:08 -0800

    add three of the minimal encodings

--- src/sud2sat.py
+++ src/sud2sat.py
@@ -5,14 +5,45 @@ import sys
 def encode(i, j, k):
     return 81*(i - 1) + 9*(j - 1) + (k - 1) + 1
 
-def get_clauses():
-    return
+def get_clauses(puzzle):
+    clauses = []
+
+    # add clauses to enforce the given values in the sudoku puzzle
+    for x in range(len(puzzle)):
+        if puzzle[x] != 0:
+            i = x // 9 + 1
+            j = x % 9 + 1
+            clauses.append([encode(i, j, puzzle[x])])
+
+    # every cell contains at least one number
+    for i in range(1, 10):
+        for j in range(1, 10):
+            clauses.append([encode(i,j,k) for k in range(1,10)])
+
+    # each number appears at most once in every row
+    for i in range(1, 10):
+        for k in range(1, 10):
+            for j in range(1, 9):
+                for l in range(j+1, 10):
+                    clauses.append([-encode(i, j, k), -encode(i, l, k)])
+
+    # each number appears at most once in every column
+    for j in range(1, 10):
+        for k in range(1, 10):
+            for i in range(1, 9):
+                for l in range(i+1, 10):
+                    clauses.append([-encode(i, j, k), -encode(l, j, k)])
+
+    # each number appears at most once in every 3x3 sub-grid
+    # TODO
+
+    return clauses
 
 '''
 This function opens the file and returns the unsolved sudoku puzzle
 into an array. The wildcard values will be always changed to 0. 
 '''
-def getfilecontents(path):
+def get_puzzle_txt(path):
     try:
         with open(path, 'r') as file:
             puzzle = file.read().strip().replace("\n", "")
@@ -23,14 +54,13 @@ def getfilecontents(path):
         print(f"Failed to parse from {path}")
         print(err)
         sys.exit(2)
-    
+
 
 def main(path):
     
-    puzzle = getfilecontents(path)
-    print("Puzzle from input:\n", puzzle, "\n")
-    clauses = get_clauses()
-    clauses = [[1,3,4,5], [14,6,3], [944, 5]] # testing
+    puzzle = get_puzzle_txt(path)
+    #print(puzzle)
+    clauses = get_clauses(puzzle)
     
     print("p cnf {} {}".format("<# variables>" , len(clauses)))
     for clause in clauses:
