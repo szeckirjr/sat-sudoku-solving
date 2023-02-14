370a4446eb0f3c54952762bfa58ab8fd20483da0
Rebecca Marshall <rebecca.marshall@shaw.ca>
2023-02-14 09:12:47 -0800

    read input from STDIN and write to STDOUT

--- src/sud2sat.py
+++ src/sud2sat.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+
+# STDIN must be in the same directory!!
 import sys
 
 
@@ -7,6 +9,7 @@ def encode(i, j, k):
 
 def get_clauses(puzzle):
     clauses = []
+    unique_variables = set()
 
     # add clauses to enforce the given values in the sudoku puzzle
     for x in range(len(puzzle)):
@@ -14,11 +17,13 @@ def get_clauses(puzzle):
             i = x // 9 + 1
             j = x % 9 + 1
             clauses.append([encode(i, j, puzzle[x])])
+            unique_variables.add(encode(i, j, puzzle[x]))
 
     # every cell contains at least one number
     for i in range(1, 10):
         for j in range(1, 10):
             clauses.append([encode(i,j,k) for k in range(1,10)])
+            unique_variables.add(encode(i,j,k) for k in range(1,10))
 
     # each number appears at most once in every row
     for i in range(1, 10):
@@ -26,6 +31,8 @@ def get_clauses(puzzle):
             for j in range(1, 9):
                 for l in range(j+1, 10):
                     clauses.append([-encode(i, j, k), -encode(i, l, k)])
+                    unique_variables.add(encode(i, j, k))
+                    unique_variables.add(encode(i, l, k))
 
     # each number appears at most once in every column
     for j in range(1, 10):
@@ -33,54 +40,39 @@ def get_clauses(puzzle):
             for i in range(1, 9):
                 for l in range(i+1, 10):
                     clauses.append([-encode(i, j, k), -encode(l, j, k)])
+                    unique_variables.add(encode(i, j, k))
+                    unique_variables.add(encode(l, j, k))
 
     # each number appears at most once in every 3x3 sub-grid
     # TODO
 
-    return clauses
+    num_variables = len(unique_variables)
+
+    return clauses, num_variables
 
 '''
-This function opens the file and returns the unsolved sudoku puzzle
-into an array. The wildcard values will be always changed to 0. 
+This function reads from STDIN and returns the unsolved sudoku puzzle
+in an array. The wildcard values will be always changed to 0. 
 '''
-def get_puzzle_txt(path):
-    try:
-        with open(path, 'r') as file:
-            puzzle = file.read().strip().replace("\n", "")
-            puzzle = [0 if char in [".", "?", "0", "*"] else int(char) for char in puzzle]
-        return puzzle
-
-    except Exception as err:
-        print(f"Failed to parse from {path}")
-        print(err)
-        sys.exit(2)
+def get_puzzle():
+    input_str = sys.stdin.read()
+    puzzle = input_str.replace("\n", "")
+    puzzle = [0 if char in [".", "?", "0", "*"] else int(char) for char in puzzle]
+    return puzzle
 
 
-def main(path):
+def main():
     
-    puzzle = get_puzzle_txt(path)
-    #print(puzzle)
-    clauses = get_clauses(puzzle)
-    
-    print("p cnf {} {}".format("<# variables>" , len(clauses)))
+    puzzle = get_puzzle()
+    clauses, num_variables = get_clauses(puzzle)
+
+    # write to STDOUT
+    print("p cnf {} {}".format(num_variables , len(clauses)))
     for clause in clauses:
         clause.append(0)
         print(*clause)
 
-# This function makes sure only 1 input file is being provided
-# Throws error if input is invalid
-def parse_args():
-    if len(sys.argv) > 2:
-        print(f"One argument expected, got {len(sys.argv) - 1}")
-        sys.exit(2)
-    elif len(sys.argv) < 2:
-        print("You must enter a sudoku puzzle in the CL")
-        sys.exit(2)
-
-    return sys.argv[1]
-
 
 if __name__ == "__main__":
-    sud = parse_args()
-    main(sud)
+    main()
     
